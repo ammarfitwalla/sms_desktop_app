7ec0b09969875c8b3ef52d9f05aaf0932fafe076
Ammar Fitwalla <ammarfitwalla@gmail.com>
2023-11-16 20:22:05 +0530

    updated main.py

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ from master_entry import MasterEntry
 if __name__ == '__main__':
     app = QApplication([])
 
-    # login_window = LoginApp()
+    login_window = LoginApp()
     master_entry_window = MasterEntry()
 
-    # if login_window.exec_() == LoginApp.Accepted:  # If login is successful
-    master_entry_window.show()
-
+    if login_window.exec_() == LoginApp.Accepted:  # If login is successful
+        master_entry_window.show()
 
     app.exec_()
     
